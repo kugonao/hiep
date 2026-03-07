545e771114174ef282bc31da997fa3e4ec4b2158
kugonao <doanunghiep999@gmail.com>
2026-03-07 07:04:22 +0700

    Add files via upload

---
+++ vb_sitekey.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Forum Runner
+ *
+ * Copyright (c) 2010-2012 to End of Time Studios, LLC
+ *
+ * This file may not be redistributed in whole or significant part.
+ *
+ * http://www.forumrunner.com
+ */
+
+$mykey = 'c66b5b31e3876e0931379f9bac7ea20c1f2f9c02';
+
+?>
---
+++ version.php
@@ -0,0 +1,33 @@
+<?php
+/*======================================================================*\
+|| #################################################################### ||
+|| # vBulletin 4.2.5 Release Candidate 1
+|| # ---------------------------------------------------------------- # ||
+|| # Copyright ©2000-2017 vBulletin Solutions Inc. All Rights Reserved. ||
+|| # This file may not be redistributed in whole or significant part. # ||
+|| # ---------------- VBULLETIN IS NOT FREE SOFTWARE ---------------- # ||
+|| #        www.vbulletin.com | www.vbulletin.com/license.html        # ||
+|| #################################################################### ||
+\*======================================================================*/
+
+/*
+ * Forum Runner
+ *
+ * Copyright (c) 2010-2011 to End of Time Studios, LLC
+ *
+ * This file may not be redistributed in whole or significant part.
+ *
+ * http://www.forumrunner.com
+ */
+
+global $fr_version, $fr_platform;
+$fr_version = '4.2.5 Release Candidate 1';
+$fr_platform = 'vb40';
+
+/*======================================================================*\
+|| ####################################################################
+|| # $Revision: 93385 $
+|| # $Date: 2017-03-15 19:52:09 -0700 (Wed, 15 Mar 2017) $
+|| ####################################################################
+\*======================================================================*/
+?>
